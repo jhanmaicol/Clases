e2b8b42f097d8f4b5c90e31e159b0946e1ce80fb
Jhanmaicol <jhanmaicol_jesus@hotmail.com>
2019-02-16 03:43:45 -0500

    Tarea - Cibertec

--- GeoQuiz/app/src/main/java/com/example/geoquiz/MainActivity.java
+++ GeoQuiz/app/src/main/java/com/example/geoquiz/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button btnSI = (Button) findViewById(R.id.buttonSI);
+
+        Button btnNO = (Button) findViewById(R.id.buttonNO);
+
+        final TextView tvResultado = (TextView) findViewById(R.id.tvResultado);
+
+        btnSI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvResultado.setText(" Usted Presiono SI y la Respuesta es:\n Correcta, Si es la Capital");
+            }
+        });
+        btnNO.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvResultado.setText(" Usted Presiono NO y la Respuesta es:\n Incorrecta, Si es la Capital");
+            }
+        });
     }
 }
