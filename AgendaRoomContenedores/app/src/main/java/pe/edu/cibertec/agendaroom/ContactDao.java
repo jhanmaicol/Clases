552b31384588d73dcb8809f83b2909a7b5550376
jhanmaicol <jhanmaicol_jesus@hotmail.com>
2019-04-06 18:29:11 -0500

    comit 1

---
+++ AgendaRoomContenedores/app/src/main/java/pe/edu/cibertec/agendaroom/ContactDao.java
@@ -0,0 +1,40 @@
+package pe.edu.cibertec.agendaroom;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+import android.database.Cursor;
+
+import java.util.List;
+
+@Dao
+public interface ContactDao {
+
+    @Query("select * from contact")
+    List<Contact> getAll();
+
+    @Query("select * from contact")
+    Cursor getAllCursor();
+
+    @Query("select * from contact where id=:id")
+    Contact getById(int id);
+
+    @Query("select * from contact where id=:id")
+    Cursor getByIdCursor(long id);
+
+    @Query("delete from contact where name=:name")
+    void deleteName(String name);
+
+    @Insert
+    void insert(Contact... contacts);
+
+    @Update
+    void update(Contact... contacts);
+
+    @Delete
+    void delete(Contact... contacts);
+
+
+}
---
+++ AgendaRoomContenedores/app/src/main/java/pe/edu/cibertec/agendaroom/MainActivity.java
@@ -0,0 +1,75 @@
+package pe.edu.cibertec.agendaroom;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView rvContact;
+    ContactAdapter adapter;
+    List<Contact> items;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        rvContact = findViewById(R.id.rvContact);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        loadItems();
+    }
+
+    private void loadItems() {
+        items = AppDatabase.getInstance(this).contactDao().getAll();
+
+        new GetAllContacsTask().execute();
+        adapter = new ContactAdapter(items);
+        rvContact.setAdapter(adapter);
+        rvContact.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent = new Intent(this, ContactActivity.class);
+        startActivity(intent);
+        return true;
+    }
+
+
+    private class GetAllContacsTask extends AsyncTask<Void,Void,List<Contact>> {
+
+        @Override
+        protected List<Contact> doInBackground(Void... voids) {
+            return AppDatabase.getInstance(MainActivity.this).contactDao().getAll();
+        }
+
+        @Override
+        protected void onPostExecute(List<Contact> contacts) {
+            super.onPostExecute(contacts);
+            items = contacts;
+
+            adapter = new ContactAdapter(items);
+           rvContact.setAdapter(adapter);
+           rvContact.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+
+        }
+    }
+}
